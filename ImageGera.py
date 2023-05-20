4934ba8439bd732a5b9d0061f741d86081d32828
João Arthur Duarte de Faria <112669326+Sertoriel@users.noreply.github.com>
2023-05-20 00:08:25 -0300

    Add files via upload

---
+++ ImageGera.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+
+API_KEY = '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'
+API_URL = 'https://api.openai.com/v1/images/generations'
+
+def generate_images(model, prompt, size, num_images, response_format='url'):
+    params = {'model': model, 'prompt': prompt, 'size': size, 'num_images': num_images, 'response_format': response_format}
+    headers = {'Authorization': f'Bearer {API_KEY}', 'Content-Type': 'application/json'}
+    try:
+        response = requests.post(API_URL, headers=headers, data=json.dumps(params))
+        data = response.json()
+        images = []
+        for url in data['data']:
+            response = requests.get(url)
+            images.append(response.content)
+        return images
+    except requests.exceptions.RequestException as e:
+        print('Ocorreu um erro durante a solicitação:', e)
+        return None
+    except KeyError as e:
+        print('A resposta da API não inclui os dados esperados:', e)
+        return None
+
+model = 'image-alpha-001'
+prompt = 'Rose field painted in blue'
+size = '1024x1024'
+num_images = 1
+
+images = generate_images(model, prompt, size, num_images)
+if images is not None:
+    for i, image in enumerate(images):
+        filename = f'futuristic_city_{i}.png'
+        with open(filename, 'wb') as f:
+            f.write(image)
